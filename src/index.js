e68864a8a247f0d99361fac5029f08cde1978184
Karolina Mamczarz <karomamczi@gmail.com>
2021-04-28 21:11:02 +0200

    feat: State in React components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: '',
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.error(error)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
-    return <div>Latitude:</div>;
+  }
+
+  render() {
+    if (this.state.errorMessage && this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading</div>;
   }
 }
 
