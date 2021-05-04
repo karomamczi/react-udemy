d73f93a3f151ad68ce8c77af2b72ff99b6b9b3e6
Karolina Mamczarz <karomamczi@gmail.com>
2021-05-04 19:03:24 +0200

    feat: Making API Requests with React

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import dogCeo from '../api/dogCeo';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = {
+    images: [],
+    fetchError: '',
+  };
+
+  onSearchSubmit = async (term) => {
+    try {
+      // Uses collection of open source dog pictures from https://dog.ceo/ API.
+      const response = await dogCeo.get(`/api/breed/${term}/images`);
+      this.setState({ images: response.data.message, fetchError: '' });
+    } catch (e) {
+      this.setState({ images: [], fetchError: e.message });
+    }
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
