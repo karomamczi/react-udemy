07ac0c9c153271d89d0a33cde247074a0a7561bd
Karolina Mamczarz <karomamczi@gmail.com>
2021-05-04 23:23:22 +0200

    feat: Using Ref's for DOM Access

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 import './ImageList.css';
 
 const ImageList = (props) => {
-  const images = props.images.map((image, index) => {
+  const images = props.images.slice(0, 100).map((image, index) => {
     return (
-      <li key={`dog-${index}`}>
-        <img alt={`Dog ${index}`} src={image} />
-      </li>
+      <ImageCard
+        key={`dog-${index}`}
+        imageAlt={`Dog ${index}`}
+        imageSrc={image}
+      />
     );
   });
   return <ul>{images}</ul>;
