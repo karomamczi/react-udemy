5119aea86a1861d1ad7e35664a02831e32a920a2
Karolina Mamczarz <karomamczi@gmail.com>
2021-05-05 22:15:25 +0200

    feat: Activate element using state

--- src/components/Accoridion.js
+++ src/components/Accoridion.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Accordion.css';
 
 const Accordion = ({ items }) => {
-  const onTitleClicked = () => {};
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClicked = (index) => {
+    setActiveIndex(index);
+  };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <li key={item.title}>
-        <div className="title active" onClick={() => onTitleClicked(index)}>
+        <div
+          className={`title ${active}`}
+          onClick={() => onTitleClicked(index)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </li>
     );
   });
 
-  return <ul className="ui styled accordion">{renderedItems}</ul>;
+  return (
+    <ul className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </ul>
+  );
 };
 
 export default Accordion;
