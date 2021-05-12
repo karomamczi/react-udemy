9464655842b2d1b0c855addedf3fcfbc30565b0b
Karolina Mamczarz <karomamczi@gmail.com>
2021-05-12 23:09:19 +0200

    feat: Translate widget

---
+++ src/components/Convert.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { key } from '../key';
+
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key,
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, debouncedText]);
+
+  return (
+    <div>
+      <h2 className="ui header">{translated}</h2>
+    </div>
+  );
+};
+
+export default Convert;
