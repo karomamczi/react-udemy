acfd688041ca5d15fa1ab389a8183417514c4a9b
Karolina Mamczarz <karomamczi@gmail.com>
2021-07-06 20:54:13 +0200

    feat: Implement new stream form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer } from 'redux-form';
 import authReducer from './authReducer';
 
 const reducers = combineReducers({
   auth: authReducer,
+  form: reducer,
 });
 
 export default reducers;
